3a30a7b392e11adb5aa6e87484be207855e0a675
paolo.bevilacqua <paolo.bevilacqua@soft.it>
2023-11-21 10:31:44 +0100

    - aggiunto metodo comando

--- main.py
+++ main.py
@@ -41,6 +41,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def bye(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    message = update.message
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}! You wrote {message.text}",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Bella Ila!")
@@ -59,6 +71,7 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("bye", bye))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
