5c1cb98ec893e6dcf1068a72c2666d21b2446e81
Emanuele Volanti <emavolant@gmail.com>
2023-11-21 12:25:11 +0100

    Add gitignore and .env support

--- main.py
+++ main.py
@@ -16,9 +16,15 @@ bot.
 """
 
 import logging
+import os
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
 
 # Enable logging
 logging.basicConfig(
@@ -66,7 +72,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("XXX").build()
+    application = Application.builder().token(TOKEN).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
