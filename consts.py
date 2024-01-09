382f2b4ee9f0487d4a5d37d2d3bc1c29bc60ad85
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2024-01-09 19:52:33 +0100

    replace placeholder error msgs with actual wording

--- consts.py
+++ consts.py
@@ -4,8 +4,6 @@ import iso639
 
 REQUIRED_DIRS = ["poses", "voice", "video"]
 
-WELCOME_MESSAGE = "Welcome to PolySignAI!"
-
 HELP_MESSAGE = """
 This bot can translate text, audio, and video between multiple signed and spolen languages!
 
@@ -43,11 +41,15 @@ ERROR_EMOJI = "\u274C"
 MSG_SHOULD_BE_VIDEO_ERROR = ERROR_EMOJI + " Selected {0} source language requires to translate from a video!"
 MSG_SHOULD_BE_TEXT_ERROR = ERROR_EMOJI + " Selected {0} source language requires to translate from an audio or a text!"
 
-MSG_WHISPER_UNABLE_TO_TRANSCRIBE = "MSG_WHISPER_UNABLE_TO_TRANSCRIBE"
-MSG_WHISPER_FAIL = "MSG_WHISPER_FAIL"
-MSG_DEEPL_FAIL = "MSG_DEEPL_FAIL"
-MSG_SIGNMT_FAIL = "MSG_SIGNMT_FAIL"
-MSG_POSE_FAIL = "MSG_POSE_FAIL"
+OPS_EMOJI = "\U0001f92d"
+NOT_AVAILABLE_EMOJI = "\U0001f636\u200D\U0001f32b\uFE0F"
+PLEASE_HANDS_EMOJI = "\U0001f64f\U0001f3fc"
+
+MSG_WHISPER_UNABLE_TO_TRANSCRIBE = f"{OPS_EMOJI} We can't hear your audio very well... could you try to speak louder, please?"
+MSG_WHISPER_FAIL = f"{NOT_AVAILABLE_EMOJI} Audio transcribing service seems to be offline, please try again later {PLEASE_HANDS_EMOJI}\n\nIn the meantime, here's a nice GIF for you..."
+MSG_DEEPL_FAIL = f"{NOT_AVAILABLE_EMOJI} Text translation service seems to be offline, please try again later {PLEASE_HANDS_EMOJI}\n\nIn the meantime, here's a nice GIF for you..."
+MSG_SIGNMT_FAIL = f"{NOT_AVAILABLE_EMOJI} Sign translation service seems to be offline, please try again later {PLEASE_HANDS_EMOJI}\n\nIn the meantime, here's a nice GIF for you..."
+MSG_POSE_FAIL = f"{OPS_EMOJI} We can't compose the translation into sign language... could you try again, please?"
 
 # If you want to add some task, you gotta add form the last position
 TASKS = ["SELECT_LANGUAGE_DST", "SELECT_LANGUAGE_SRC"]
@@ -62,6 +64,7 @@ SPANISH_FLAG_EMOJI = "\U0001f1ea\U0001f1f8"
 US_FLAG_EMOJI      = "\U0001f1fa\U0001f1f8"
 
 OPEN_HANDS_EMOJI = "\U0001f450"
+SPEAK_EMOJI = "\U0001f5e3\uFE0F"
 
 HOURGLASS_EMOJI = "\u23F3"
 
@@ -89,11 +92,11 @@ DST = "destination"
 TEXT_TO_SIGNED_BASE_URL = "https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose"
 
 KEYBOARD_LANG_LIST = [
-    {"text": f"Italian {ITALIAN_FLAG_EMOJI}", "is_spoken": True},
-    {"text": f"English {ENGLISH_FLAG_EMOJI}", "is_spoken": True},
-    {"text": f"French {FRENCH_FLAG_EMOJI}", "is_spoken": True},
-    {"text": f"German {GERMAN_FLAG_EMOJI}", "is_spoken": True},
-    {"text": f"Spanish {SPANISH_FLAG_EMOJI}", "is_spoken": True},
+    {"text": f"Italian {ITALIAN_FLAG_EMOJI}{SPEAK_EMOJI}", "is_spoken": True},
+    {"text": f"English {ENGLISH_FLAG_EMOJI}{SPEAK_EMOJI}", "is_spoken": True},
+    {"text": f"French {FRENCH_FLAG_EMOJI}{SPEAK_EMOJI}", "is_spoken": True},
+    {"text": f"German {GERMAN_FLAG_EMOJI}{SPEAK_EMOJI}", "is_spoken": True},
+    {"text": f"Spanish {SPANISH_FLAG_EMOJI}{SPEAK_EMOJI}", "is_spoken": True},
     {"text": f"Italian Sign Lang{ITALIAN_FLAG_EMOJI}{OPEN_HANDS_EMOJI}", "is_spoken": False},
     {"text": f"British Sign Lang{ENGLISH_FLAG_EMOJI}{OPEN_HANDS_EMOJI}", "is_spoken": False},
     {"text": f"German Sign Lang{GERMAN_FLAG_EMOJI}{OPEN_HANDS_EMOJI}", "is_spoken": False},
@@ -102,11 +105,11 @@ KEYBOARD_LANG_LIST = [
 ]
 
 LANGUAGE_DICT = {
-    f"Italian {ITALIAN_FLAG_EMOJI}": "it",
-    f"English {ENGLISH_FLAG_EMOJI}": "en",
-    f"French {FRENCH_FLAG_EMOJI}": "fr",
-    f"German {GERMAN_FLAG_EMOJI}": "de",
-    f"Spanish {SPANISH_FLAG_EMOJI}": "es",
+    f"Italian {ITALIAN_FLAG_EMOJI}{SPEAK_EMOJI}": "it",
+    f"English {ENGLISH_FLAG_EMOJI}{SPEAK_EMOJI}": "en",
+    f"French {FRENCH_FLAG_EMOJI}{SPEAK_EMOJI}": "fr",
+    f"German {GERMAN_FLAG_EMOJI}{SPEAK_EMOJI}": "de",
+    f"Spanish {SPANISH_FLAG_EMOJI}{SPEAK_EMOJI}": "es",
     f"Italian Sign Lang{ITALIAN_FLAG_EMOJI}{OPEN_HANDS_EMOJI}": "ise", # This "language code" is used by sign.mt API
     f"British Sign Lang{ENGLISH_FLAG_EMOJI}{OPEN_HANDS_EMOJI}": "bfi",
     f"German Sign Lang{GERMAN_FLAG_EMOJI}{OPEN_HANDS_EMOJI}": "gsg",
@@ -115,11 +118,11 @@ LANGUAGE_DICT = {
 }
 
 LANGUAGE_DICT_REVERSED = {
-    "it": f"Italian {ITALIAN_FLAG_EMOJI}",
-    "en": f"English {ENGLISH_FLAG_EMOJI}",
-    "fr": f"French {FRENCH_FLAG_EMOJI}",
-    "de": f"German {GERMAN_FLAG_EMOJI}",
-    "es": f"Spanish {SPANISH_FLAG_EMOJI}",
+    "it": f"Italian {ITALIAN_FLAG_EMOJI}{SPEAK_EMOJI}",
+    "en": f"English {ENGLISH_FLAG_EMOJI}{SPEAK_EMOJI}",
+    "fr": f"French {FRENCH_FLAG_EMOJI}{SPEAK_EMOJI}",
+    "de": f"German {GERMAN_FLAG_EMOJI}{SPEAK_EMOJI}",
+    "es": f"Spanish {SPANISH_FLAG_EMOJI}{SPEAK_EMOJI}",
     "ise": f"Italian Sign Lang{ITALIAN_FLAG_EMOJI}{OPEN_HANDS_EMOJI}",
     "bfi": f"British Sign Lang{ENGLISH_FLAG_EMOJI}{OPEN_HANDS_EMOJI}",
     "gsg": f"German Sign Lang{GERMAN_FLAG_EMOJI}{OPEN_HANDS_EMOJI}",
@@ -160,3 +163,11 @@ SENTENCES = [
     "Fortune favors the bold.",
     "Rome wasn't built in a day."
 ]
+
+HI_HAND = "\U0001f596\U0001f3fc"
+HEART_EMOJI = "\U0001f9e1"
+
+NEWLINE = "\n"
+WELCOME_MESSAGE = f"{HI_HAND} Welcome to PolySignAI, a Telegram Bot to perform spoken-to-sign, sign-to-spoken and sign-to-sign translations in the following languages:\n{NEWLINE.join(list(LANGUAGE_DICT.keys()))}\n\n"\
+    f"Use the command /help to see all details about available Bot commands.\n\n"\
+    f"Thank you for choosing us {HEART_EMOJI}"
